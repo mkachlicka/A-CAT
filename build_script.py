b29520a478d977f014b400a8feae147bb1c64b5e
Heyuan Zeng <hi@universe.observer>
2024-01-11 12:42:00 +0000

    chore: change CI build script

---
+++ build_script.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import PyInstaller.__main__
+
+HERE = Path(__file__).parent.absolute()
+path_to_main = str(HERE / "src" / "acat" / "__main__.py")
+
+
+def test_build():
+    PyInstaller.__main__.run(
+        [
+            path_to_main,
+            "--onefile",
+            "--windowed",
+            "--onedir",
+            "-y",
+            "--log-level=DEBUG",
+            "--clean",
+        ]
+    )
--- test-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-poetry run pyinstaller src/acat/__main__.py --name A-CAT --onefile --windowed --onedir -y --log-level=WARN --clean
