c618d9feb1960f9367d33d6d23e7af2f1559a43f
Heyuan Zeng <hi@universe.observer>
2024-01-20 00:09:22 +0000

    feat: add help text

--- src/acat/ui/help_window.py
+++ src/acat/ui/help_window.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QTextEdit, QVBoxLayout, QWidget
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QLabel, QVBoxLayout, QWidget
 
 
 class HelpWindow(QWidget):
@@ -10,8 +11,29 @@ class HelpWindow(QWidget):
         self.setWindowTitle("Help Window")
 
         layout = QVBoxLayout()
-        text_edit = QTextEdit()
-        text_edit.setPlainText("Help Message 1\nHelp Message 2\nHelp Message 3")
-        text_edit.setReadOnly(True)
-        layout.addWidget(text_edit)
+        # Heading
+        heading = QLabel("<h2>Automated Comprehensibility Assessment Tool Usage</h2>")
+        layout.addWidget(heading)
+
+        # Bullet Points
+        bullet_points = QLabel(
+            """
+                    <ul>
+                        <li>Choose one or multiple ".wav" audio files using the choose button on the top left; if you don't have the files right now and wants to try this app, you can click the "Load Sample" button.</li>
+                        <li>Click "Judge All" to evaluate all chosen files, or click the "Judge" button for each individual file.</li>
+                        <li>Check out the judged scores in the table below and you can click the "Info" button for each file to see detailed explanations.</li>
+                        <li>You can export the results as a CSV file using the "Export" Button.</li>
+                    </ul>
+                """
+        )
+        bullet_points.setTextFormat(Qt.TextFormat.RichText)
+        layout.addWidget(bullet_points)
+
+        # Hyperlink Paragraph
+        hyperlink_paragraph = QLabel(
+            'For more information, please visit <a href="https://link.here">the application website</a>.'
+        )
+        hyperlink_paragraph.setOpenExternalLinks(True)
+        layout.addWidget(hyperlink_paragraph)
+
         self.setLayout(layout)
